c80ae4b5521987de4c9347e3208c50a02cb83c53
Nate Mills <mills.nate@gmail.com>
2018-03-01 15:33:35 -0800

    Updating to use tortoise diff window

--- Commands/svnShowChangesCommand.py
+++ Commands/svnShowChangesCommand.py
@@ -12,14 +12,15 @@ class svnShowChangesCommand(sublime_plugin.TextCommand, svnController):
         self.svnDir = self.get_scoped_path('file');
 
         procText = self.run_svn_command([ "svn", "diff", self.svnDir]);
+        procText = self.run_svn_command([], False, 'diff', self.svnDir);
 
-        if len(procText):
-            newView = sublime.active_window().new_file();
-            newView.insert(edit, 0, procText);
-            newView.set_syntax_file("Packages/Diff/Diff.tmLanguage");
-            newView.set_scratch(True);
-        else:
-            sublime.status_message("The files match.");
+#         if len(procText):
+#             newView = sublime.active_window().new_file();
+#             newView.insert(edit, 0, procText);
+#             newView.set_syntax_file("Packages/Diff/Diff.tmLanguage");
+#             newView.set_scratch(True);
+#         else:
+#             sublime.status_message("The files match.");
 
     def is_enabled(self):
         return len(str(self.get_svn_dir())) != 0
